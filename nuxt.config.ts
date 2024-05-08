50dffe5d0af13cca26ca9fd5d01a57fb896e8300
meysam kiani <dev.mysm@gmail.com>
2024-05-08 23:34:14 +0330

    add prefix lang to url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/i18n", '@pinia/nuxt'],
+  modules: ["@nuxtjs/i18n", "@pinia/nuxt"],
   srcDir: "src",
   i18n: {
-    locales: [{ code: "en", iso: "en_US", file: "en.json" }],
+    locales: [
+      { code: "en", iso: "en_US", file: "en.json" },
+      { code: "fa", iso: "fa_IR", file: "fa.json" },
+    ],
     defaultLocale: "en",
     langDir: "locales/",
+    strategy: "prefix",
     lazy: true,
     customRoutes: "config",
-  }
+  },
 });
